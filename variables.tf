547052bd19c40787e96dc37390f3a39ad93e7a6b
Mudit Pathak <mudit.pathak@coforge.com>
2023-11-01 13:44:38 +0530

    Change list to string

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "subnet_id" {
   description = "Subnet deatials for EFS"
-  default     = ["subnet-0b86a94123ccf1094"]
-  type        = list(any)
+  default     = "subnet-0b86a94123ccf1094"
+  type        = string
 }
 
 
 variable "security_groups_id" {
   description = "Security Group Ids for EFS mount"
-  default     = ["sg-0c5426001fd0fb679"]
-  type        = list(any)
+  default     = "sg-0c5426001fd0fb679"
+  type        = string
 }
